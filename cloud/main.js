9b727a1a00cc4910b1d85023764a3fce64389de9
Charles128 <charles.amoako.128@gmail.com>
2014-05-20 14:59:46 -0400

    create assets directory in public dir. remove ejs view engine

--- cloud/main.js
+++ cloud/main.js
@@ -8,7 +8,7 @@
 var express = require('express');
 var app = express();
 
-app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', function (req, res){
   res.render('index')
